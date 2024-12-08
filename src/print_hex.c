342ac1a8fc06f75e802c76c0c1bfa566bed343f3
bobbyskywalker <aleksander.garbacz@gmail.com>
2024-12-08 10:15:46 +0100

    print hex done

--- src/print_hex.c
+++ src/print_hex.c
@@ -13,47 +13,33 @@
 #include "../includes/printf.h"
 #include "../lib/libft/libft.h"
 
+// function calculates the len of a potential
+// hexstring based off an int argument
+// bitwise right shift calculates the length
+// +3 is used for rounding up
 static int hexstr_length(unsigned int n) {
-    if (n == 0) 
+    int bit_length;
+
+	bit_length = 0;
+	if (!n) 
         return 1;
-    int bit_length = 0;
     while (n > 0) {
         bit_length++;
         n >>= 1;
     }
     return (bit_length + 3) / 4;
 }
-// TODO: negative values
-// case param: 1 - lowercase, 2 - uppercase
-void	print_hex(int arg, int case_p)
-{
-	char	*hex_dict;
-	char	*res;
-	int		mod;
-	int		i;
 
-	res = ft_calloc(hexstr_length(arg) + 1, sizeof(char));
-	if (!res)
-		return ;
+static int calc_hexstr(char *res, int arg, int case_p) 
+{
+	char *hex_dict;
+	int	mod;
+	int i;
+
 	if (case_p == 1)
 		hex_dict = "0123456789abcdef";
 	else
 		hex_dict = "0123456789ABCDEF";
-	if (!arg)
-	{
-		write(1, "0", 1);
-		return ;
-	}
-	else if (arg < 0)
-	{
-		if (arg == INT_MIN)
-		{
-			write(1, "-2147483648", 11);
-			return ;
-		}
-		write(1, "-", 1);
-		arg = -arg;
-	}
 	i = 0;
 	while (arg)
     {
@@ -62,7 +48,35 @@ void	print_hex(int arg, int case_p)
         arg /= 16;
         i++;
     }
-    while (i > 0)
+    return (i);
+}
+
+// case param: 1 - lowercase, 2 - uppercase
+void	print_hex(int arg, int case_p)
+{
+	char	*res;
+	int		i;
+
+	if (!arg)
+	{
+		write(1, "0", 1);
+		return ;
+	} 
+	else if (arg == INT_MIN)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (arg < 0)
+	{
+		write(1, "-", 1);
+		arg = -arg;
+	}
+	res = ft_calloc(hexstr_length(arg) + 1, sizeof(char));
+	if (!res)
+		return ;
+	i = calc_hexstr(res, arg, case_p);
+	while (i > 0)
 		write(1, &res[--i], 1);
 	free(res);
 }
